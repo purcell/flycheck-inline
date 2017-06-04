db625638954af28518753918b5c180c4825688ac
stardiviner <numbchild@gmail.com>
2017-06-04 14:40:26 +0800

    require flycheck

--- flycheck-inline.el
+++ flycheck-inline.el
@@ -1,7 +1,7 @@
 ;;; flycheck-inline.el --- Display flycheck error message with inline popup style.
 
 ;; Authors: stardiviner <numbchild@gmail.com>
-;; Package-Requires: ((emacs "24.4") (cl-lib "0.3") (inline-docs "1.0.1"))
+;; Package-Requires: ((emacs "24.4") (cl-lib "0.3") (inline-docs "1.0.1") (flycheck "31-cvs"))
 ;; Package-Version: 0.1
 ;; Keywords: tools convenience
 ;; homepage: https://github.com/stardiviner/flycheck-inline
@@ -16,6 +16,7 @@
 ;;; Code:
 ;;; ----------------------------------------------------------------------------
 (require 'inline-docs)
+(require 'flycheck)
 
 (defun flycheck-inline (errors)
   "Display the flycheck message of ERRORS with inline style."
@@ -31,8 +32,7 @@
   "This is a minor mode to display flycheck error message in inline way."
   :global t
   (if flycheck-inline-mode
-      (with-eval-after-load 'flycheck
-        (setq flycheck-display-errors-function 'flycheck-inline))
+      (setq flycheck-display-errors-function 'flycheck-inline)
     (setq flycheck-display-errors-function 'flycheck-display-error-messages)
     ))
 
